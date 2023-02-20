6ab51d75090e160f3ba72977472233263db9cdd0
Md Masud Rana <mdmasude7@gmail.com>
2023-02-20 15:20:38 +0600

    asd

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function run() {
   //mongodb connection start
 
   const uri =
-    "mongodb+srv://masudrana:kzeQ6jEvvmQoGiUl@cluster0.hsazmle.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://masudrana:Uaz0fxaLqIXO0JgH@cluster0.ynspdjq.mongodb.net/?retryWrites=true&w=majority";
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -22,10 +22,27 @@ function run() {
     // collections
 
     const basaCollection = client.db("basaBhara").collection("allBasa");
+    const usersCollection = client.db("basaBhara").collection("users");
 
     app.get("/text", (req, res) => {
       res.send("hello world");
     });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      // const result = await usersCollection.insertOne(user);
+      // res.send(result);
+    });
+
+    // get users collection
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //
   } finally {
   }
 
