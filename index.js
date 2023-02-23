e46d9f702d34d7aa947b838b71dfa5abcf0d462d
Md Masud Rana <mdmasude7@gmail.com>
2023-02-23 16:36:55 +0600

    delete basa

--- index.js
+++ index.js
@@ -85,6 +85,14 @@ function run() {
       res.send(result);
     });
 
+    // delete basaCollection
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await basaCollection.deleteOne(query);
+      res.send(result);
+    });
     //
 
     //
